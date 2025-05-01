83e0b294e721511d9d198d024ef261d5cafcd80c
5pzl <68611270+5pzl@users.noreply.github.com>
2025-05-01 02:52:49 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Ensure Tailwind scans your files
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Scans your files for Tailwind classes
   theme: {
     extend: {
       colors: {
