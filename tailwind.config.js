7428f3db4501822cd8a81af5a961584de415f104
5pzl <68611270+5pzl@users.noreply.github.com>
2025-05-01 02:13:03 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-green": "#001a1a",
+        "teal-green": "#0b2e2e",
+        "deep-teal": "#002b1f",
+        "light-teal": "#112622",
+        "yellow-accent": "#fddf4a",
+      },
+      boxShadow: {
+        glow: "0 0 20px 5px rgba(253, 223, 74, 0.5)",
+      },
+    },
   },
   plugins: [],
 };
